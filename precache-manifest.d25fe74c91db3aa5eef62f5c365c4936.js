92e7d0ee4330e99fb5393fd9316ca48aabf05bc5
Malak Joseph <malak_joseph2010@yahoo.com>
2018-11-06 07:21:13 +0200

    Updates

---
+++ precache-manifest.d25fe74c91db3aa5eef62f5c365c4936.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "20979a94d5ecf676684d1df7f2844d8b",
+    "url": "/outlet-pages/static/media/y1.20979a94.jpg"
+  },
+  {
+    "revision": "2fe3dca411c0f82d9990",
+    "url": "/outlet-pages/static/css/main.3ae4e7b5.chunk.css"
+  },
+  {
+    "revision": "f2bd2b7c5b770d4bdef8",
+    "url": "/outlet-pages/static/js/1.f2bd2b7c.chunk.js"
+  },
+  {
+    "revision": "73ec90b0c5ccddf4f07d",
+    "url": "/outlet-pages/static/js/runtime~main.73ec90b0.js"
+  },
+  {
+    "revision": "a1becc6f58adbcd29e4d206bb0d181f5",
+    "url": "/outlet-pages/static/media/logo.a1becc6f.png"
+  },
+  {
+    "revision": "124d2febe1a399905e6d4f6367d5be97",
+    "url": "/outlet-pages/static/media/x.124d2feb.jpg"
+  },
+  {
+    "revision": "2fe3dca411c0f82d9990",
+    "url": "/outlet-pages/static/js/main.2fe3dca4.chunk.js"
+  },
+  {
+    "revision": "3f1fda88172d96a4d6af55b9f18d5706",
+    "url": "/outlet-pages/static/media/y2.3f1fda88.jpg"
+  },
+  {
+    "revision": "91f3d5731406d620704179c775933c6b",
+    "url": "/outlet-pages/static/media/card1.91f3d573.jpg"
+  },
+  {
+    "revision": "986c73e1eec09893b4452894bdc1aeb4",
+    "url": "/outlet-pages/static/media/card2.986c73e1.jpg"
+  },
+  {
+    "revision": "94cf609658b174565a387c530a52e058",
+    "url": "/outlet-pages/static/media/card3.94cf6096.jpg"
+  },
+  {
+    "revision": "1732d02daf575fe4db50cd308a0c816f",
+    "url": "/outlet-pages/static/media/4683.1732d02d.jpg"
+  },
+  {
+    "revision": "551c5bef850d4603a7dbd492fd4a7466",
+    "url": "/outlet-pages/index.html"
+  }
+];
